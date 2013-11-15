29ff818ef57356ba66b73d1968fd1a619d1ffded
Paul Lloyd <paulrobertlloyd@Pinova.local>
2013-11-15 00:39:48 +0000

    Content is King

--- pages/topics/topicname/index.php
+++ pages/topics/topicname/index.php
@@ -9,7 +9,7 @@
     <main class="main" role="main">
         <section class="index">
             <header class="preface">
-                <h1 class="preface_title" data-icon="&#x2654;">Business</h1>
+                <h1 class="preface_title" data-icon="&#x2655;">Business</h1>
                 <div class="preface_main">
                     <p class="lede">Where there&#8217;s muck, there are clients, deliverables and contracts. Occasionally, there&#8217;s brass. Here are articles on developing and improving relationships with clients; managing projects, both business and personal; writing contracts and presenting your work; and encouraging success and avoiding failure.</p>
                 </div>
