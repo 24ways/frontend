2f58d51fa976e8084d9428bb76ee84e38726356a
Paul Lloyd <paulrobertlloyd@Pinova.local>
2013-11-24 00:28:53 +0000

    Add animated corners (thanks @joshje!)

--- patterns/partials/structure/foot.php
+++ patterns/partials/structure/foot.php
@@ -1,7 +1,6 @@
 	<script src="/_assets/js/vendor/adactio/progresponsive/progresponsive.js"></script>
 	<script src="/_assets/js/vendor/nathanford/data-href/data-href.js"></script>
 	<script src="/_assets/js/vendor/leaverou/prismjs/prism.js"></script>
-
 	<script src="/_assets/js/vendor/filamentgroup/shoestring/shoestring.js"></script>
 	<script>jQuery = shoestring;</script>
 	<script src="/_assets/js/vendor/filamentgroup/ajaxinclude/ajaxinclude.js"></script>
@@ -10,6 +9,19 @@
 			e.preventDefault();
 			$(this).removeAttr("data-interaction").ajaxInclude();
 		});
+
+		// Corner reveal
+		var cornerOver = function(){
+			this.className = this.className.replace(' js-mouseout', '');
+		};
+		var cornerOut = function(){
+			this.className += ' js-mouseout';
+		};
+		var corners = document.querySelectorAll('.summary-article');
+		for (var i = corners.length - 1; i >= 0; i--) {
+			corners[i].addEventListener('js-mouseover', cornerOver, false);
+			corners[i].addEventListener('js-mouseout', cornerOut, false);
+		};
 	</script>
 </body>
 </html>
