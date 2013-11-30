a53e54ebaf4f5ded1c9d3e9c84d79aa0835c2281
Paul Lloyd <paulrobertlloyd@Pinova.local>
2013-11-30 15:30:24 +0000

    Try to find consistent positioning style for author avatar images

--- pages/authors/authorname/index.php
+++ pages/authors/authorname/index.php
@@ -11,10 +11,12 @@
 			<header class="preface h-card">
 				<h1 class="preface_title p-name">Drew McLellan</h1>
 				<div class="preface_main">
-					<p class="lede p-note">
-						<span class="avatar"><img class="preface_image u-photo" src="http://media.24ways.org/authors/drewmclellan280.jpg" width="280" height="280" alt="Drew McLellan"/></span>
-						Drew McLellan is lead developer on your favourite small <span class="caps">CMS</span>, <a href="http://grabaperch.com/">Perch</a>. He is Director and Senior Developer at UK-based web development agency edgeofmyseat.com, and formerly Group Lead at the Web Standards Project. When not publishing 24 ways, Drew keeps a <a class="u-url" href="http://allinthehead.com/">personal site</a> covering web development issues and themes, <a href="http://flickr.com/drewm/">takes photos</a> and <a href="http://twitter.com/drewm">tweets a lot</a>.
-					</p>
+					<div class="lede p-note">
+						<span class="avatar"><img class="preface_image u-photo" src="http://cloud.24ways.org/authors/rachelandrew280.jpg" width="280" height="280" alt="Rachel Andrew"></span>
+						<p><strong class="fn n">Rachel Andrew</strong> is a Director of edgeofmyseat.com, a UK web development consultancy and creators of the small content management system, <a href="http://grabaperch.com">Perch</a>. She is the author of a number of web design and development books including The CSS3 Anthology for SitePoint and the <a href="http://www.fivesimplesteps.com/pages/pocket-guides">CSS3 Layout Modules Pocket Guide</a> to be published by Five Simple Steps in January 2013.</p>
+						<p>When not writing about business and technology on her blog at <a href="http://rachelandrew.co.uk">rachelandrew.co.uk</a> or <a href="http://lanyrd.com/profile/rachelandrew/">speaking at conferences</a>, you will usually find Rachel out for a run, as she has a London Marathon place for 2013.</p>
+						<p class="photo-credit">Photo: <a href="http://duncandavidson.com/">James Duncan Davidson</a><br></p>
+					</div>
 				</div>
 			</header><!--/.preface-->
 
