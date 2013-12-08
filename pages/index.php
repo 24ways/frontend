457b8251513aa4a63d3de46dfcdf762a3e1af7f1
Paul Lloyd <paulrobertlloyd@Pinova.local>
2013-12-08 20:33:26 +0000

    WAVE: Remove suspicious link text on article summaries

--- pages/index.php
+++ pages/index.php
@@ -29,7 +29,7 @@
 						</header>
 						<div class="summary_main">
 							<p><a href="http://jontangerine.com/">Jon Tan</a> opens the final door on the web advent calendar to reveal the glittering star of research evidence shining its light on design decisions. Merry Christmas!</p>
-							<a class="summary_link" href="/pages/YYYY/article/">Continue reading&#8230;</a>
+							<a class="summary_link" href="/pages/YYYY/article/">Impress your friends with &#8216;&#8217;</a>
 						</div>
 						<footer class="summary_footer">
 							<p class="summary_author p-author h-card">
@@ -51,7 +51,7 @@
 						</header>
 						<div class="summary_main">
 							<p><a href="http://stuffandnonsense.co.uk/">Andy Clarke</a> unpicks issues of cost and value, specifically when clients question our prices. It can be uncomfortable, but there&#8217;s always wiggle room. Bear left? Monkey right.</p>
-							<a class="summary_link" href="/pages/YYYY/article/">Continue reading&#8230;</a>
+							<a class="summary_link" href="/pages/YYYY/article/">Impress your friends with &#8216;Monkey Business&#8217;</a>
 						</div>
 						<footer class="summary_footer">
 							<p class="summary_author p-author h-card">
@@ -73,7 +73,7 @@
 						</header>
 						<div class="summary_main">
 							<p><a href="http://maban.co.uk/">Anna Debenham</a> harnesses the console browser huskies to the sled of web design and races off into the deeply forested landscape, leaving in her wake only an in-depth analysis of the new Wii U and its internet capabilities.</p>
-							<a class="summary_link" href="/pages/YYYY/article/">Continue reading&#8230;</a>
+							<a class="summary_link" href="/pages/YYYY/article/">Impress your friends with &#8216;Unwrapping the Wii U Browser&#8217;</a>
 						</div>
 						<footer class="summary_footer">
 							<p class="summary_author p-author h-card">
@@ -95,7 +95,7 @@
 						</header>
 						<div class="summary_main">
 							<p><a href="http://nathanperetic.com/">Nathan Peretic</a> weighs the page refresh in the balance and finds it wanting – wanting to be brought up to date, that is, with some future <span class="caps">CSS</span> sparkle.</p>
-							<a class="summary_link" href="/pages/YYYY/article/">Continue reading&#8230;</a>
+							<a class="summary_link" href="/pages/YYYY/article/">Impress your friends with &#8216;Infinite Canvas: Moving Beyond the Page&#8217;</a>
 						</div>
 						<footer class="summary_footer">
 							<p class="summary_author p-author h-card">
@@ -117,7 +117,7 @@
 						</header>
 						<div class="summary_main">
 							<p><a href="http://incisive.nu/">Erin Kissane</a> sets off hopefully into the broad open plains of the new year, vanquishing gooey and chaotic content with a refreshing dose of clear-headed strategy to prevent your post-Christmas content hangover.</p>
-							<a class="summary_link" href="/pages/YYYY/article/">Continue reading&#8230;</a>
+							<a class="summary_link" href="/pages/YYYY/article/">Impress your friends with &#8216;Content Planning Demystified&#8217;</a>
 						</div>
 						<footer class="summary_footer">
 							<p class="summary_author p-author h-card">
@@ -139,7 +139,7 @@
 						</header>
 						<div class="summary_main">
 							<p><a href="http://suda.co.uk/">Brian Suda</a> packs his compass and map (well, his smartphone) to guide us all home for Christmas. And there&#8217;ll be one jolly fellow who&#8217;ll find this little web app useful on that annual 24-hour world trip.</p>
-							<a class="summary_link" href="/pages/YYYY/article/">Continue reading&#8230;</a>
+							<a class="summary_link" href="/pages/YYYY/article/">Impress your friends with &#8216;Direction, Distance and Destinations&#8217;</a>
 						</div>
 						<footer class="summary_footer">
 							<p class="summary_author p-author h-card">
@@ -161,7 +161,7 @@
 						</header>
 						<div class="summary_main">
 							<p><a href="http://rachelandrew.co.uk/">Rachel Andrew</a> unwraps the CSS3 grid layout module and sets out how its new properties can break the ties between source order and layout, and rescue us from the quaking ground of floats. Support is limited to IE10 right now, but Christmas and New Year are times for looking forward to the future.</p>
-							<a class="summary_link" href="/pages/YYYY/article/">Continue reading&#8230;</a>
+							<a class="summary_link" href="/pages/YYYY/article/">Impress your friends with &#8216;Giving Content Priority with CSS3 Grid Layout&#8217;</a>
 						</div>
 						<footer class="summary_footer">
 							<p class="summary_author p-author h-card">
@@ -183,7 +183,7 @@
 						</header>
 						<div class="summary_main">
 							<p><a href="http://brendandawes.com/">Brendan Dawes</a> raids his Christmas stocking early and shares some interesting and useful code snippets for you to grab, remix and combine into new things for 2013.</p>
-							<a class="summary_link" href="/pages/YYYY/article/">Continue reading&#8230;</a>
+							<a class="summary_link" href="/pages/YYYY/article/">Impress your friends with &#8216;Cut Copy Paste&#8217;</a>
 						</div>
 						<footer class="summary_footer">
 							<p class="summary_author p-author h-card">
@@ -205,7 +205,7 @@
 						</header>
 						<div class="summary_main">
 							<p><a href="http://anna.ps/">Anna Powell-Smith</a> stirs the silver sixpence of design into the Christmas pudding of web development with some shortcuts and advice to help improve the skills of programmers intimidated by visual design.</p>
-							<a class="summary_link" href="/pages/YYYY/article/">Continue reading&#8230;</a>
+							<a class="summary_link" href="/pages/YYYY/article/">Impress your friends with &#8216;How to Make Your Site Look Half-Decent in Half an Hour&#8217;</a>
 						</div>
 						<footer class="summary_footer">
 							<p class="summary_author p-author h-card">
@@ -227,7 +227,7 @@
 						</header>
 						<div class="summary_main">
 							<p><a href="http://artequalswork.com/">Nathan Ford</a> delves deep into the sack of <span class="caps">CSS</span> goodies and rewards our attention with a clever object-oriented, scalable and modular approach to attribute selectors. Old wine in new bottles.</p>
-							<a class="summary_link" href="/pages/YYYY/article/">Continue reading&#8230;</a>
+							<a class="summary_link" href="/pages/YYYY/article/">Impress your friends with &#8216;A Harder-Working Class&#8217;</a>
 						</div>
 						<footer class="summary_footer">
 							<p class="summary_author p-author h-card">
@@ -250,7 +250,7 @@
 						</header>
 						<div class="summary_main">
 							<p><a href="http://markboultondesign.com/">Emma Boulton</a> doesn&#8217;t let a good question about designing and using surveys as part of a project&#8217;s research activities go unanswered. Q: Have you been good this year? A: Yes|No. Think carefully. Santa knows the answer.</p>
-							<a class="summary_link" href="/pages/YYYY/article/">Continue reading&#8230;</a>
+							<a class="summary_link" href="/pages/YYYY/article/">Impress your friends with &#8216;Using Questionnaires for Design Research&#8217;</a>
 						</div>
 						<footer class="summary_footer">
 							<p class="summary_author p-author h-card">
@@ -272,7 +272,7 @@
 						</header>
 						<div class="summary_main">
 							<p><a href="http://lesjames.com/">Les James</a> proposes an alternative to the fully fluid grid as an approach to responsive layout challenges. Sprinkle on some Sass fairy dust and, providing you&#8217;ve been good this year, watch your creation spring to life.</p>
-							<a class="summary_link" href="/pages/YYYY/article/">Continue reading&#8230;</a>
+							<a class="summary_link" href="/pages/YYYY/article/">Impress your friends with &#8216;Redesigning the Media Query&#8217;</a>
 						</div>
 						<footer class="summary_footer">
 							<p class="summary_author p-author h-card">
@@ -294,7 +294,7 @@
 						</header>
 						<div class="summary_main">
 							<p><a href="http://laurakalbag.com/">Laura Kalbag</a> beckons us in from the cold wastelands of transitional, device-rooted layouts to warm our toes at the hearth of a more systematic way of working.</p>
-							<a class="summary_link" href="/pages/YYYY/article/">Continue reading&#8230;</a>
+							<a class="summary_link" href="/pages/YYYY/article/">Impress your friends with &#8216;Design Systems&#8217;</a>
 						</div>
 						<footer class="summary_footer">
 							<p class="summary_author p-author h-card">
@@ -316,7 +316,7 @@
 						</header>
 						<div class="summary_main">
 							<p><a href="http://paulrobertlloyd.com/">Paul Robert Lloyd</a> engages with the two main approaches to the matter of responsive images and finds them wanting. Could &#8220;Bah, humbug!&#8221; be a reasonable response to markup excess?</p>
-							<a class="summary_link" href="/pages/YYYY/article/">Continue reading&#8230;</a>
+							<a class="summary_link" href="/pages/YYYY/article/">Impress your friends with &#8216;Responsive Images: What We Thought We Needed&#8217;</a>
 						</div>
 						<footer class="summary_footer">
 							<p class="summary_author p-author h-card">
@@ -338,7 +338,7 @@
 						</header>
 						<div class="summary_main">
 							<p><a href="http://rcottrell.com/">Rebecca Cottrell</a> speeds through the dark landscape of web wireframes towards the snowy slopes of early prototypes with glittering animations and transitions that show your developing product at its best.</p>
-							<a class="summary_link" href="/pages/YYYY/article/">Continue reading&#8230;</a>
+							<a class="summary_link" href="/pages/YYYY/article/">Impress your friends with &#8216;Fluent Design through Early Prototyping&#8217;</a>
 						</div>
 						<footer class="summary_footer">
 							<p class="summary_author p-author h-card">
@@ -360,7 +360,7 @@
 						</header>
 						<div class="summary_main">
 							<p><a href="http://hereinthehive.com/">Dan Donald</a> conjures up the ghost of <del>Christmas</del> <ins>the Web</ins> Yet To Come through the possibilities offered by the contextual data available to us from web-enabled devices.</p>
-							<a class="summary_link" href="/pages/YYYY/article/">Continue reading&#8230;</a>
+							<a class="summary_link" href="/pages/YYYY/article/">Impress your friends with &#8216;Should We Be Reactive?&#8217;</a>
 						</div>
 						<footer class="summary_footer">
 							<p class="summary_author p-author h-card">
@@ -382,7 +382,7 @@
 						</header>
 						<div class="summary_main">
 							<p><a href="http://www.valhead.com/">Val Head</a> marshals overexcited <span class="caps">CSS</span> transitions and animations, which are like naughty children elbowing their way out of the presentation layer and into the behaviour grotto to get at the goodies before Christmas. Santa will be pleased!</p>
-							<a class="summary_link" href="/pages/YYYY/article/">Continue reading&#8230;</a>
+							<a class="summary_link" href="/pages/YYYY/article/">Impress your friends with &#8216;Giving CSS Animations and Transitions Their Place&#8217;</a>
 						</div>
 						<footer class="summary_footer">
 							<p class="summary_author p-author h-card">
@@ -404,7 +404,7 @@
 						</header>
 						<div class="summary_main">
 							<p><a href="http://fulljames.net/">Stephen Fulljames</a> places the reindeers of thought before the sleigh of action, encouraging coders everywhere to plan ahead when implementing JavaScript libraries.</p>
-							<a class="summary_link" href="/pages/YYYY/article/">Continue reading&#8230;</a>
+							<a class="summary_link" href="/pages/YYYY/article/">Impress your friends with &#8216;Think First, Code Later&#8217;</a>
 						</div>
 						<footer class="summary_footer">
 							<p class="summary_author p-author h-card">
@@ -426,7 +426,7 @@
 						</header>
 						<div class="summary_main">
 							<p><a href="http://www.rachelnabors.com/">Rachel Nabors</a> brings together the web&#8217;s three Magi – <span class="caps">HTML</span>, <span class="caps">CSS</span> and JavaScript – to create seamless, soundtracked animations in the browser.</p>
-							<a class="summary_link" href="/pages/YYYY/article/">Continue reading&#8230;</a>
+							<a class="summary_link" href="/pages/YYYY/article/">Impress your friends with &#8216;Flashless Animation&#8217;</a>
 						</div>
 						<footer class="summary_footer">
 							<p class="summary_author p-author h-card">
@@ -448,7 +448,7 @@
 						</header>
 						<div class="summary_main">
 							<p><a href="http://timkadlec.com/">Tim Kadlec</a> broadens the scope of responsive web design to include bandwidth and hardware capabilities. Images too big? Another JS library? It&#8217;s time to work off the seasonal weight gain from your responsive website.</p>
-							<a class="summary_link" href="/pages/YYYY/article/">Continue reading&#8230;</a>
+							<a class="summary_link" href="/pages/YYYY/article/">Impress your friends with &#8216;Responsive Responsive Design&#8217;</a>
 						</div>
 						<footer class="summary_footer">
 							<p class="summary_author p-author h-card">
@@ -470,7 +470,7 @@
 						</header>
 						<div class="summary_main">
 							<p><a href="http://hellogeri.com/">Geri Coady</a> extends goodwill to all with some insights about colour and how it impacts everyone using our sites and apps. Full of practical tips and tools, this gift keeps on giving.</p>
-							<a class="summary_link" href="/pages/YYYY/article/">Continue reading&#8230;</a>
+							<a class="summary_link" href="/pages/YYYY/article/">Impress your friends with &#8216;Colour Accessibility&#8217;</a>
 						</div>
 						<footer class="summary_footer">
 							<p class="summary_author p-author h-card">
@@ -492,7 +492,7 @@
 						</header>
 						<div class="summary_main">
 							<p><a href="http://trentwalton.com/">Trent Walton</a> celebrates the collaborative <span class="caps">DNA</span> of the web community. We make the web better when we work together, and we should seek out ways of contributing for the greater good. A little bit like Christmas, really.</p>
-							<a class="summary_link" href="/pages/YYYY/article/">Continue reading&#8230;</a>
+							<a class="summary_link" href="/pages/YYYY/article/">Impress your friends with &#8216;Being Prepared To Contribute&#8217;</a>
 						</div>
 						<footer class="summary_footer">
 							<p class="summary_author p-author h-card">
@@ -514,7 +514,7 @@
 						</header>
 						<div class="summary_main">
 							<p><a href="http://heckhouse.com/">Bethany Heck</a> slips warming cloves and spices into the web designer&#8217;s mulled wine by sharing some of the methods she uses to encourage and maintain creative success in new projects.</p>
-							<a class="summary_link" href="/pages/YYYY/article/">Continue reading&#8230;</a>
+							<a class="summary_link" href="/pages/YYYY/article/">Impress your friends with &#8216;Starting Your Project on the Right Foot (and Keeping It There)&#8217;</a>
 						</div>
 						<footer class="summary_footer">
 							<p class="summary_author p-author h-card">
@@ -536,7 +536,7 @@
 						</header>
 						<div class="summary_main">
 							<p><a href="http://allinthehead.com/">Drew McLellan</a> invites you to pull up to the 2012 24 ways bumper, baby, with an neat JavaScript solution to an HTML5 &lt;video&gt; branding problem. And that was &#8220;24 ways bumper&#8221; not &#8220;Christmas jumper&#8221;. He has enough of those already.</p>
-							<a class="summary_link" href="/pages/YYYY/article/">Continue reading&#8230;</a>
+							<a class="summary_link" href="/pages/YYYY/article/">Impress your friends with &#8216;HTML5 Video Bumpers&#8217;</a>
 						</div>
 						<footer class="summary_footer">
 							<p class="summary_author p-author h-card">
