78d41e95d0132eb16ea23917c3cf00a674ba0c17
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2016-11-23 01:56:41 +0000

    A11y fixes, bugs squashed etc.

--- src/components/global/menu/menu.js
+++ src/components/global/menu/menu.js
@@ -5,16 +5,39 @@
 
   var $ = document.querySelector.bind(document);
   var $$ = document.querySelectorAll.bind(document);
-  var menuToggle = $('a[href="#menu"]');
+  var menuToggleEl = $('[href="#navigation"]');
+  var navigationDrawerID = 'navigation__drawer';
+  var navigationDrawerEl = $('#navigation__drawer');
 
-  var toggleMenu = function (elem, on, off) {
-    elem.setAttribute('data-menu', elem.getAttribute('data-menu') === on ? off : on);
+  var toggleMenu = function(state) {
+    menuToggleEl.setAttribute('aria-expanded', state);
+    document.body.setAttribute('data-menu-expanded', state);
+    navigationDrawerEl.setAttribute('aria-hidden', !state);
   };
 
-  if (menuToggle) {
-    menuToggle.addEventListener('click', function(e) {
-      toggleMenu(document.body, 'closed', 'open');
+  if (menuToggleEl) {
+    // Setup menu button
+    menuToggleEl.setAttribute('role', 'button');
+    menuToggleEl.setAttribute('aria-controls', navigationDrawerID);
+    menuToggleEl.setAttribute('aria-expanded', false);
+
+    // Setup navigation drawer
+    navigationDrawerEl.setAttribute('aria-hidden', true);
+
+    // Toggle menu expanded/collapsed
+    menuToggleEl.addEventListener('click', function(e) {
+      var state = menuToggleEl.getAttribute('aria-expanded') === 'false' ? true : false;
+
+      toggleMenu(state);
+
       e.preventDefault();
+    });
+
+    // Close menu if escape key is pressed
+    window.addEventListener('keyup', function(e) {
+      if (e.keyCode == 27) {
+        toggleMenu(false);
+      }
     });
   }
 }());
