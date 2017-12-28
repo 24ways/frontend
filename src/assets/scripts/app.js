2c1f0240c109e91e7e876b2faae68a9eb7f7ae5c
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2017-12-28 13:52:32 +0000

    Move JS detection to head element

--- src/assets/scripts/app.js
+++ src/assets/scripts/app.js
@@ -2,10 +2,6 @@
 import loadWebfonts from './modules/webfont-loader';
 import menu from './modules/menu';
 
-// Flag JS as being supported
-const docEl = document.documentElement;
-docEl.className = docEl.className.replace('no-js', 'has-js');
-
 // Run
 menu();
 loadWebfonts();
